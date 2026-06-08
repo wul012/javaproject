195f218271948d95e4fff3c21ae582de61316849
wul <2494447558@qq.com>
2026-06-08 10:17:23 +0800

    v905 expose signed approval artifact draft instruction preflight foundation routes

---
+++ advanced-order-platform/src/main/java/com/codexdemo/orderplatform/ops/OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightFoundationController.java
@@ -0,0 +1,62 @@
+package com.codexdemo.orderplatform.ops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(OpsShardReadinessRoutePaths.BASE_PATH)
+public class OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightFoundationController {
+
+    private final OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightCatalogService
+            catalogService;
+    private final OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightDigestInstructionService
+            digestInstructionService;
+    private final OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightOperatorInstructionService
+            operatorInstructionService;
+    private final OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightSignatureInstructionService
+            signatureInstructionService;
+
+    public OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightFoundationController(
+            OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightCatalogService
+                    catalogService,
+            OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightDigestInstructionService
+                    digestInstructionService,
+            OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightOperatorInstructionService
+                    operatorInstructionService,
+            OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightSignatureInstructionService
+                    signatureInstructionService
+    ) {
+        this.catalogService = catalogService;
+        this.digestInstructionService = digestInstructionService;
+        this.operatorInstructionService = operatorInstructionService;
+        this.signatureInstructionService = signatureInstructionService;
+    }
+
+    @GetMapping(OpsShardReadinessRoutePaths
+            .OPERATOR_EVIDENCE_VALUE_SUPPLY_SIGNED_APPROVAL_ARTIFACT_DRAFT_INSTRUCTION_PREFLIGHT_CATALOG)
+    public OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightResponse catalog() {
+        return catalogService.catalog();
+    }
+
+    @GetMapping(OpsShardReadinessRoutePaths
+            .OPERATOR_EVIDENCE_VALUE_SUPPLY_SIGNED_APPROVAL_ARTIFACT_DRAFT_INSTRUCTION_PREFLIGHT_DIGEST_INSTRUCTIONS)
+    public OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightResponse
+    digestInstructions() {
+        return digestInstructionService.digestInstructions();
+    }
+
+    @GetMapping(OpsShardReadinessRoutePaths
+            .OPERATOR_EVIDENCE_VALUE_SUPPLY_SIGNED_APPROVAL_ARTIFACT_DRAFT_INSTRUCTION_PREFLIGHT_OPERATOR_INSTRUCTIONS)
+    public OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightResponse
+    operatorInstructions() {
+        return operatorInstructionService.operatorInstructions();
+    }
+
+    @GetMapping(OpsShardReadinessRoutePaths
+            .OPERATOR_EVIDENCE_VALUE_SUPPLY_SIGNED_APPROVAL_ARTIFACT_DRAFT_INSTRUCTION_PREFLIGHT_SIGNATURE_INSTRUCTIONS)
+    public OpsShardReadinessOperatorEvidenceValueSupplySignedApprovalArtifactDraftInstructionPreflightResponse
+    signatureInstructions() {
+        return signatureInstructionService.signatureInstructions();
+    }
+}
