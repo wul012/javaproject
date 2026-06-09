d842df4d8b292be980e36473ae5039d7ea52f823
wul <2494447558@qq.com>
2026-06-09 08:11:26 +0800

    v1115 add candidate document submission precheck controller

---
+++ advanced-order-platform/src/main/java/com/codexdemo/orderplatform/ops/OpsShardReadinessCandidateDocumentSubmissionPrecheckController.java
@@ -0,0 +1,23 @@
+package com.codexdemo.orderplatform.ops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(OpsShardReadinessRoutePaths.BASE_PATH)
+public class OpsShardReadinessCandidateDocumentSubmissionPrecheckController {
+
+    private final OpsShardReadinessCandidateDocumentSubmissionPrecheckService service;
+
+    public OpsShardReadinessCandidateDocumentSubmissionPrecheckController(
+            OpsShardReadinessCandidateDocumentSubmissionPrecheckService service
+    ) {
+        this.service = service;
+    }
+
+    @GetMapping(OpsShardReadinessRoutePaths.CANDIDATE_DOCUMENT_SUBMISSION_PRECHECK)
+    public OpsShardReadinessCandidateDocumentSubmissionPrecheckResponse precheck() {
+        return service.precheck();
+    }
+}
