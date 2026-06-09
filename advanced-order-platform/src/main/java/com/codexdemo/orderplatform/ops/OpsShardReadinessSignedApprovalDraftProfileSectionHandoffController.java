95dab972b7273d2ad6b982ffc750f7976bbc9f94
wul <2494447558@qq.com>
2026-06-09 15:33:59 +0800

    v1249 add signed approval draft profile section handoff controller

---
+++ advanced-order-platform/src/main/java/com/codexdemo/orderplatform/ops/OpsShardReadinessSignedApprovalDraftProfileSectionHandoffController.java
@@ -0,0 +1,23 @@
+package com.codexdemo.orderplatform.ops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(OpsShardReadinessRoutePaths.BASE_PATH)
+public class OpsShardReadinessSignedApprovalDraftProfileSectionHandoffController {
+
+    private final OpsShardReadinessSignedApprovalDraftProfileSectionHandoffService service;
+
+    public OpsShardReadinessSignedApprovalDraftProfileSectionHandoffController(
+            OpsShardReadinessSignedApprovalDraftProfileSectionHandoffService service
+    ) {
+        this.service = service;
+    }
+
+    @GetMapping(OpsShardReadinessRoutePaths.SIGNED_APPROVAL_DRAFT_PROFILE_SECTION_HANDOFF)
+    public OpsShardReadinessSignedApprovalDraftProfileSectionHandoffResponse handoff() {
+        return service.handoff();
+    }
+}
