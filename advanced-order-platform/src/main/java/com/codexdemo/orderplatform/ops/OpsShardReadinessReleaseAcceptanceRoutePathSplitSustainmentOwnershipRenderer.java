b314d4cf49c324bb39811e2fe9899f2d38a30e91
wul <2494447558@qq.com>
2026-06-10 22:23:18 +0800

    v1592 add route path split sustainment ownership renderer

---
+++ advanced-order-platform/src/main/java/com/codexdemo/orderplatform/ops/OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentOwnershipRenderer.java
@@ -0,0 +1,21 @@
+package com.codexdemo.orderplatform.ops;
+
+import java.util.List;
+
+final class OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentOwnershipRenderer {
+
+    private OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentOwnershipRenderer() {
+    }
+
+    static OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentResponse.MarkdownSection render(
+            List<OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentResponse.OwnershipRule> rules
+    ) {
+        return OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentRendererSupport.section(
+                "Ownership Rules",
+                rules.stream()
+                        .map(rule -> "- " + rule.component() + " owner=" + rule.owner()
+                                + " landing=" + rule.landingZone() + " enforced=" + rule.enforced())
+                        .toList()
+        );
+    }
+}
