734398141d2da5827e1b4b899f45166f1336a637
wul <2494447558@qq.com>
2026-06-10 22:23:21 +0800

    v1598 add route path split sustainment registry renderer

---
+++ advanced-order-platform/src/main/java/com/codexdemo/orderplatform/ops/OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentRenderer.java
@@ -0,0 +1,33 @@
+package com.codexdemo.orderplatform.ops;
+
+import java.util.List;
+
+final class OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentRenderer {
+
+    private OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentRenderer() {
+    }
+
+    static List<OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentResponse.MarkdownSection> render(
+            List<OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentResponse.SourceSnapshot>
+                    sourceSnapshots,
+            List<OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentResponse.OwnershipRule>
+                    ownershipRules,
+            List<OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentResponse.DriftGuard> driftGuards,
+            List<OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentResponse.BoundaryGuard>
+                    boundaryGuards,
+            List<OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentResponse.CiGate> ciGates,
+            List<OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentResponse.ConsumerHandoff>
+                    consumerHandoffs,
+            List<OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentResponse.ScorecardEntry> scorecard
+    ) {
+        return List.of(
+                OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentSourceRenderer.render(sourceSnapshots),
+                OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentOwnershipRenderer.render(ownershipRules),
+                OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentDriftRenderer.render(driftGuards),
+                OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentBoundaryRenderer.render(boundaryGuards),
+                OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentCiRenderer.render(ciGates),
+                OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentConsumerRenderer.render(consumerHandoffs),
+                OpsShardReadinessReleaseAcceptanceRoutePathSplitSustainmentScorecardRenderer.render(scorecard)
+        );
+    }
+}
