681a0c8f67a3345ba7ee74d2ad97aca0406c1749
wul <2494447558@qq.com>
2026-06-11 13:39:33 +0800

    v1664 add credential resolver disabled fake harness archive controller

---
+++ advanced-order-platform/src/main/java/com/codexdemo/orderplatform/ops/OpsShardReadinessCredentialResolverDisabledFakeHarnessEvidenceArchiveController.java
@@ -0,0 +1,24 @@
+package com.codexdemo.orderplatform.ops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(OpsShardReadinessCredentialResolverRoutePaths.BASE_PATH)
+public class OpsShardReadinessCredentialResolverDisabledFakeHarnessEvidenceArchiveController {
+
+    private final OpsShardReadinessCredentialResolverDisabledFakeHarnessEvidenceArchiveService service;
+
+    public OpsShardReadinessCredentialResolverDisabledFakeHarnessEvidenceArchiveController(
+            OpsShardReadinessCredentialResolverDisabledFakeHarnessEvidenceArchiveService service
+    ) {
+        this.service = service;
+    }
+
+    @GetMapping(OpsShardReadinessCredentialResolverRoutePaths
+            .CREDENTIAL_RESOLVER_DISABLED_FAKE_HARNESS_EVIDENCE_ARCHIVE)
+    public OpsShardReadinessCredentialResolverDisabledFakeHarnessEvidenceArchiveResponse archive() {
+        return service.archive();
+    }
+}
