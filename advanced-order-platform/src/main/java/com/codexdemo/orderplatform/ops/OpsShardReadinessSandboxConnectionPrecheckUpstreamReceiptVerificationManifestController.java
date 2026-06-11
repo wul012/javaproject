d15f563f746f2b84da2da2c4eb0614bfa131956b
wul <2494447558@qq.com>
2026-06-11 16:13:55 +0800

    v1701 add sandbox connection precheck upstream receipt manifest controller

---
+++ advanced-order-platform/src/main/java/com/codexdemo/orderplatform/ops/OpsShardReadinessSandboxConnectionPrecheckUpstreamReceiptVerificationManifestController.java
@@ -0,0 +1,24 @@
+package com.codexdemo.orderplatform.ops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(OpsShardReadinessSandboxConnectionRoutePaths.BASE_PATH)
+public class OpsShardReadinessSandboxConnectionPrecheckUpstreamReceiptVerificationManifestController {
+
+    private final OpsShardReadinessSandboxConnectionPrecheckUpstreamReceiptVerificationManifestService service;
+
+    public OpsShardReadinessSandboxConnectionPrecheckUpstreamReceiptVerificationManifestController(
+            OpsShardReadinessSandboxConnectionPrecheckUpstreamReceiptVerificationManifestService service
+    ) {
+        this.service = service;
+    }
+
+    @GetMapping(OpsShardReadinessSandboxConnectionRoutePaths
+            .SANDBOX_CONNECTION_PRECHECK_UPSTREAM_RECEIPT_VERIFICATION_MANIFEST)
+    public OpsShardReadinessSandboxConnectionPrecheckUpstreamReceiptVerificationManifestResponse manifest() {
+        return service.manifest();
+    }
+}
